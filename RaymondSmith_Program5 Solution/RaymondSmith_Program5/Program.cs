3a0712a61f6fc717b5f3a8ca58be453c6649c5f9
Raymond <stich12@aol.com>
2015-02-23 12:24:42 -0500

    First commit\Reader Class added

---
+++ RaymondSmith_Program5 Solution/RaymondSmith_Program5/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RaymondSmith_Program5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string file = @"C:\CPT244\sudoku-bad-1.txt";
+            int[,] sdku = new int[9, 9];
+
+            Reader.readSodukuFile(file, sdku);
+
+            for (int r = 0; r < 9; r++)
+            {
+                for (int c = 0; c < 9; c++)
+                {
+                    Console.Write(sdku[r, c] + " ");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ RaymondSmith_Program5 Solution/RaymondSmith_Program5/Reader.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RaymondSmith_Program5
+{
+    class Reader
+    {
+        public static void readSodukuFile(string file, int[,] array)
+        {
+            StreamReader reader = new StreamReader(new FileStream(file, FileMode.Open, FileAccess.Read));
+            {
+
+                char[] delim = { ',' };
+
+                for (int row = 0; row < 9; row++)          
+                {          
+                        string line = reader.ReadLine();
+                        string[] fields = line.Split(delim);
+
+                        for (int i = 0; i < fields.Length; i++)
+                        {
+                            array[row, i] = int.Parse(fields[i]);
+                        }                    
+                }
+            }
+
+
+        }
+    }
+}
