1acb07bbc944fffb5803d2c2244506bf3057edd9
Raymond <stich12@aol.com>
2015-02-25 02:24:33 -0500

    Create Column and Row checkers w/ structure to store errors

---
+++ RaymondSmith_Program5 Solution/RaymondSmith_Program5/Checker.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RaymondSmith_Program5
+{
+    class Checker
+    {
+        public static bool checkColumns(int[,] sdku)
+        {
+            bool valid = true;
+
+            for (int col = 0; col < ; col++)
+            {
+                ArrayList input = new ArrayList();
+                
+                for (int row = 0; row < ; row++)
+
+            }
+
+
+
+                return valid;
+        }
+    }
+}
--- RaymondSmith_Program5 Solution/RaymondSmith_Program5/Program.cs
+++ RaymondSmith_Program5 Solution/RaymondSmith_Program5/Program.cs
@@ -8,22 +8,18 @@ namespace RaymondSmith_Program5
 {
     class Program
     {
+        public const int  SODUKU_SIZE = 9;
+
         static void Main(string[] args)
         {
+            
             string file = @"C:\CPT244\sudoku-bad-1.txt";
             int[,] sdku = new int[9, 9];
+            
 
             Reader.readSodukuFile(file, sdku);
 
-            for (int r = 0; r < 9; r++)
-            {
-                for (int c = 0; c < 9; c++)
-                {
-                    Console.Write(sdku[r, c] + " ");
-                }
-
-                Console.WriteLine();
-            }
+            
         }
     }
 }
